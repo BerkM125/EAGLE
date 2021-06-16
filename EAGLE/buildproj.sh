9cdd011988ed8304d0f64f88713e085dba724385
BerkM125 <49427416+BerkM125@users.noreply.github.com>
2021-06-16 08:32:46 -0700

    Update buildproj.sh

--- EAGLE/buildproj.sh
+++ EAGLE/buildproj.sh
@@ -1,2 +1,2 @@
-g++ -o CarBall.out gameprocess.cpp renderloop.cpp physics.cpp figures.cpp figureslib.hpp interface.cpp transformations.cpp interfacelib.hpp physicslib.hpp cameralib.hpp camera.cpp -lGL -lglut -lGLU
-./CarBall.out
+g++ -o Eagle.out gameprocess.cpp renderloop.cpp physics.cpp figures.cpp figureslib.hpp interface.cpp transformations.cpp interfacelib.hpp physicslib.hpp cameralib.hpp camera.cpp -lGL -lglut -lGLU
+./Eagle.out
